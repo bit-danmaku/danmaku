abdb8c5d225744ac9b8f5da3cbb582d629693a8b
XunDiYang <1920310178@qq.com>
2021-12-06 20:11:35 +0800

    在kafka-consumer中添加PostDanmaku

--- kafka-consumer/handler/kafka-consumer.go
+++ kafka-consumer/handler/kafka-consumer.go
@@ -2,17 +2,35 @@ package handler
 
 import (
 	"context"
+	"github.com/bit-danmaku/danmaku/common/model"
 
 	log "github.com/asim/go-micro/v3/logger"
-
 	pb "github.com/bit-danmaku/danmaku/proto/kafkaconsumer"
 )
 
-type KafkaConsumer struct{}
+type KafkaConsumer struct{
+	dbConnector *model.DBConnector
+}
 
-func (e *KafkaConsumer) Call(ctx context.Context, req *pb.CallRequest, rsp *pb.CallResponse) error {
-	log.Infof("Received KafkaConsumer.Call request: %v", req)
-	rsp.Msg = "Hello " + req.Name
+
+func (kc *KafkaConsumer) PostDanmaku(ctx context.Context, req *pb.PostRequest, rsp *pb.PostResponse) error {
+	log.Infof("Received DanmakuCache.GetDanmakuListByChannel request: %+v", req)
+
+	danmaku := req.Danmaku
+
+	err := kc.dbConnector.AddDanmaku(ctx, model.Danmaku{
+		ChannelID: req.ChannelID,
+		Author:    danmaku.Author,
+		Time:      danmaku.Time,
+		Text:      danmaku.Text,
+		Color:     danmaku.Color,
+		Type:      uint8(danmaku.Type),
+	})
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
